73be7f28a9ee954b53e4ecb4734ff6cc455d7eaa
AllanHyr <djaleial@outlook.fr>
2023-06-04 21:50:31 +0200

    Réparation euros dollars

--- test.php
+++ test.php
@@ -1,15 +1,28 @@
 <?php
 
-$to = "allan.bouguerab@gmail.com";
-$subject = "Test d\'envoi de mail";
-$message = "Ceci est un test d\'envoi de mail via PHP.";
+$euro = NULL;
 
-$headers = "Content-Type: text/plain; charset=utf-8\r\n";
-$headers .= "From: mytoolbox769@gmail.com\r\n";
+$currency = $euro === null ? 'USD' : 'EUR';
+$reverseCurrency = $currency === 'EUR' ? 'USD' : 'EUR';
 
-if(mail($to, $subject, $message, $headers)) {
-    echo 'Le message a été envoyé avec succès.';
-} else {
-    echo 'Une erreur est survenue lors de l\'envoi du message.';
+$url = 'https://open.er-api.com/v6/latest/' . $currency;
+
+$data = file_get_contents($url);
+$data = json_decode($data, true);
+
+$rate = $data['rates'][$reverseCurrency];
+
+if($euro === null){
+    $euro = $dollars * $rate;
+    return [
+        'EUR' => $euro,
+    ];
 }
+if($dollars === null){
+    $dollars = $euro * $rate;
+    return [
+        'USD' => $dollars,
+    ];
+}
+
 ?>
